209bdf9f3bcf9162614cd47a654e7361d5a13c03
Matt Vinall <matt.vinall7@gmail.com>
2025-06-17 12:24:53 -0400

    update to eslintrc file

--- functions/.eslintrc.js
+++ functions/.eslintrc.js
@@ -1,5 +1,5 @@
 module.exports = {
-  root: true,
+  root: true, // This is the most important line, it stops ESLint from looking in parent folders.
   env: {
     es6: true,
     node: true,
@@ -14,20 +14,18 @@ module.exports = {
   ],
   parser: "@typescript-eslint/parser",
   parserOptions: {
-    project: ["tsconfig.json", "tsconfig.dev.json"],
+    project: ["tsconfig.json"],
     sourceType: "module",
+    tsconfigRootDir: __dirname,
   },
   ignorePatterns: [
-    "/lib/**/*", // Ignore built files.
-    "/generated/**/*", // Ignore generated files.
+    "/lib/**/*", // Ignore the compiled Javascript files
   ],
-  plugins: [
-    "@typescript-eslint",
-    "import",
-  ],
+  plugins: ["@typescript-eslint", "import"],
   rules: {
     "quotes": ["error", "double"],
     "import/no-unresolved": 0,
     "indent": ["error", 2],
+    "require-jsdoc": 0, // Not necessary for this project
   },
 };
